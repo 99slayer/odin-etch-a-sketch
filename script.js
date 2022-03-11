f1a6c33c1d4796544b96ec940fdb99c5a56db624
auz <lankyauz@gmail.com>
2022-03-11 12:07:18 -0700

    Made a function that creates and appends 16 grid spaces (divs) to the rows we created earlier. Then loop that function 16 times to fill each row.

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-let gridContainer = document.querySelector('.gridContainer');
+const gridContainer = document.querySelector('.gridContainer');
 
 for(let i=1;i<=16;i++){
     const gridRow = document.createElement('div');
     gridRow.classList.add(`gridRow-${i}`,'row');
     gridContainer.appendChild(gridRow);
 }
-// for(let i=1;i<=256;i++){
-//     const gridSpace = document.createElement('div');
-//     gridSpace.classList.add(`gridSpace-${i}`,'space');
-// }
+const rowNodeList = document.querySelectorAll('.row');
+
+function appendToRow(a){
+    for(let i=1;i<=16;i++){
+        const gridSpace = document.createElement('div');
+        gridSpace.classList.add(`gridSpace-${i}`,'space',`row-${a}`);
+        rowNodeList[a].appendChild(gridSpace);
+    }
+}
+for(let i=1;i<=16;i++){
+    appendToRow(i-1);
+}
