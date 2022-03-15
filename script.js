3d07e1203bfa4c0d35e0fe8d11c11eb898868982
auz <lankyauz@gmail.com>
2022-03-15 13:59:41 -0600

    Grid creation is now done using a function that takes user input, allowing for different grid sizes. The grid creation function makes use of another function that removes the previous grid before the new grid is made. The 'drawing' (hover color change), is currently broken due to the above changes.

--- script.js
+++ script.js
@@ -1,26 +1,51 @@
+let color = 'black';
+
+const clearCanvas = document.querySelector('.clearCanvas');
+const canvasSize = document.querySelector('.canvasSize');
 const gridContainer = document.querySelector('.gridContainer');
 
-for(let i=1;i<=16;i++){
-    const gridRow = document.createElement('div');
-    gridRow.classList.add(`gridRow-${i}`,'row');
-    gridContainer.appendChild(gridRow);
-}
-const rowNodeList = document.querySelectorAll('.row');
+canvasSize.addEventListener('click',()=>{
+    gridSize = prompt('Grid size?');
+    gridConstruction(gridSize);
+});
 
-function appendToRow(a){
-    for(let i=1;i<=16;i++){
-        const gridSpace = document.createElement('div');
-        gridSpace.classList.add(`gridSpace-${i}`,'space',`row-${a + 1}`);
-        rowNodeList[a].appendChild(gridSpace);
+function clearChildren(x){
+    while(x.firstChild){
+        x.removeChild(x.firstChild);
     }
 }
-for(let i=1;i<=16;i++){
-    appendToRow(i-1);
+
+function gridConstruction(x){
+    clearChildren(gridContainer);
+    for(let i=1;i<=x;i++){
+        const gridRow = document.createElement('div');
+        gridRow.classList.add(`gridRow-${i}`,'row');
+        gridContainer.appendChild(gridRow);
+    }
+    const rowNodeList = document.querySelectorAll('.row');
+    
+    function appendToRow(a){
+        for(let i=1;i<=x;i++){
+            const gridSpace = document.createElement('div');
+            gridSpace.classList.add(`gridSpace-${i}`,'space',`row-${a + 1}`);
+            rowNodeList[a].appendChild(gridSpace);
+        }
+    }
+    for(let i=1;i<=x;i++){
+        appendToRow(i-1);
+    }
 }
+gridConstruction(16);
 
 const spaceNodeList = document.querySelectorAll('.space');
 
-spaceNodeList.forEach((node) => {
-    node.addEventListener('mouseover', () => {
-        node.style.backgroundColor = 'black'});
+spaceNodeList.forEach((space) => {
+    space.addEventListener('mouseover', () => {
+        space.style.backgroundColor = `${color}`});
 });
+
+clearCanvas.addEventListener('click',()=>{
+    spaceNodeList.forEach((space) =>{
+        space.style.backgroundColor = 'white';
+    })
+})
