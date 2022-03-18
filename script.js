6db53fb2079234fb6f1d7332d62de45008f33adb
auz <lankyauz@gmail.com>
2022-03-18 17:33:38 -0600

    Got shading mode working.

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-let color = 'black';
+const defaultColor = 'rgb(0,0,0)';
+let mode;
+let color;
 let spaceNodeList;
 let mouseDown = false;
 window.onmousedown=()=>{mouseDown=true};
@@ -6,10 +8,26 @@ window.onmouseup=()=>{mouseDown=false};
 
 const clearCanvas = document.querySelector('.clearCanvas');
 const canvasSize = document.querySelector('.canvasSize');
+const defaultModeBtn = document.querySelector('.defaultMode');
+const shadingModeBtn = document.querySelector('.shadingMode');
 const gridContainer = document.querySelector('.gridContainer');
 
+defaultModeBtn.addEventListener('click',()=>{
+    mode = 'default';
+    console.log(mode);
+});
+shadingModeBtn.addEventListener('click',()=>{
+    mode = 'shading';
+    console.log(mode);
+});
+
+// ----------------TURN INTO FUNCTIONS?--------------------
 canvasSize.addEventListener('click',()=>{
     gridSize = prompt('Grid size?');
+    if(gridSize>100){
+        alert('please choose a grid size of 100 or less.')
+        return;
+    };
     gridConstruction(gridSize);
 });
 clearCanvas.addEventListener('click',()=>{
@@ -17,6 +35,7 @@ clearCanvas.addEventListener('click',()=>{
         space.style.backgroundColor = 'white';
     });
 });
+// -------------------------------------------------------
 
 function clearChildren(x){
     while(x.firstChild){
@@ -46,17 +65,37 @@ function gridConstruction(x){
     spaceNodeList = document.querySelectorAll('.space');
 
     spaceNodeList.forEach((space)=>{
-        space.addEventListener('mousedown',changeColor);
-        space.addEventListener('mouseover',changeColor);
+        space.style.backgroundColor = 'rgb(255,255,255)'
+        space.addEventListener('mousedown',draw);
+        space.addEventListener('mouseover',draw);
     });
 }
 
-function changeColor(e){
+function draw(e){
     // can i reverse this?
-    if(e.type==='mouseover'&&!mouseDown){return}
-    else{
-        e.target.style.backgroundColor = `${color}`;
+    let x = e;
+    if(x.type==='mouseover'&&!mouseDown){return}
+    else if(mode=='shading'){
+        shadingMode(x);
     }
+    else{
+        defaultMode(x);
+    };
+};
+
+function shadingMode(i){
+    let value = i.target.style.backgroundColor;
+    value = value.replace('rgb(','')
+    value = parseInt(value);
+    if(value>0){
+        i.target.style.backgroundColor = `rgb(${value - 25.5},${value - 25.5},${value - 25.5})`;
+    }
+}
+function defaultMode(i){
+    i.target.style.backgroundColor = `${defaultColor}`;
 };
 
 gridConstruction(16);
+
+// Research button tags in HTML and how you can make a JavaScript function run when one is clicked.
+// make buttons run functions instead of using event listeners? probably less code 'x'
